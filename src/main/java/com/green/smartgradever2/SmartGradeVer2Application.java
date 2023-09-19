cafd295e086236c2c7bd960bb653323bb72a0c72
devitoolz <tkdfuf0923@gmail.com>
2023-09-19 11:33:32 +0900

    feat: CORS 설정

--- src/main/java/com/green/smartgradever2/SmartGradeVer2Application.java
+++ src/main/java/com/green/smartgradever2/SmartGradeVer2Application.java
@@ -2,12 +2,28 @@ package com.green.smartgradever2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SmartGradeVer2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartGradeVer2Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("https://web-smart-grade-2rrqq2blmpn233b.sel5.cloudtype.app");
+            }
+        };
     }
 
 }
